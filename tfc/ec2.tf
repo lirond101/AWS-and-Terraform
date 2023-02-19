988b959f5117740bdbee1407adb1d7aab91bf097
lirond101 <lirond101@gmail.com>
2023-02-19 14:00:26 +0000

    fix var issue

--- tfc/ec2.tf
+++ tfc/ec2.tf
@@ -31,6 +31,7 @@ module "my_ec2" {
   ami_nginx                        = data.aws_ami.ubuntu-18.id
   instance_type_nginx              = var.instance_type
   iam_instance_profile_nginx       = aws_iam_instance_profile.instance_profile.name
+  encrypted_disk_device_name_nginx = var.nginx_encrypted_disk_device_name
   key_name                         = var.key_name
   user_data_nginx                  = templatefile("${path.module}/startup_script.tpl", {
                                       vpc_cidr_block = module.my_vpc.vpc_cidr_block
